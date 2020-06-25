a764c172657f51695a1b5e6c236e2047209149da
MrWalshyType2 <morgan.walsh@academytrainee.com>
2020-06-25 11:07:30 +0100

    Changed printString() to returnString() method

--- src/com/qa/FirstApp/HelloWorld.java
+++ src/com/qa/FirstApp/HelloWorld.java
@@ -1,12 +1,12 @@
 package com.qa.FirstApp;
 
 public class HelloWorld {
-	public static void printMessage(String message) {
-		System.out.println(message);
+	public static String returnString(String message) {
+		return message;
 	}
 	
 	public static void main(String args[]) {
 		String message = "Hello World!";
-		printMessage(message);
+		System.out.println(returnString(message));
 	}
 }
