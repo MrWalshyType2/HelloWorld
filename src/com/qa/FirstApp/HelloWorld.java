fd268eec2780c5e28210159011ce7eb7cf76e1ae
MrWalshyType2 <morgan.walsh@academytrainee.com>
2020-06-25 11:04:46 +0100

    Added printMessage function

--- src/com/qa/FirstApp/HelloWorld.java
+++ src/com/qa/FirstApp/HelloWorld.java
@@ -1,8 +1,12 @@
 package com.qa.FirstApp;
 
 public class HelloWorld {
+	public static void printMessage(String message) {
+		System.out.println(message);
+	}
+	
 	public static void main(String args[]) {
 		String message = "Hello World!";
-		System.out.println(message);
+		printMessage(message);
 	}
 }
